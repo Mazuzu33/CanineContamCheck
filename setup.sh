d83e5a509c16ed71267b60a6a91682896348ec53
Mazuki <mazuki.wu@gmail.com>
2026-06-23 11:12:59 -0500

    Add setup script for downloading from s3

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -l
+wget -P data/reference_genomes/UU_Cfam_GSD_1.0_ROSY https://s3.msi.umn.edu/caninecontamcheck/ref_gen/UU_Cfam_GSD_1.0_ROSY/UU_Cfam_GSD_1.0_ROSY.fa.gz
+wget -P data/reference_genomes/UU_Cfam_GSD_1.0_ROSY https://s3.msi.umn.edu/caninecontamcheck/ref_gen/UU_Cfam_GSD_1.0_ROSY/UU_Cfam_GSD_1.0_ROSY.fa.gz.fai 
