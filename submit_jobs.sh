4e9a279122fa7a4614823ba1e6a827d1d2340202
Mazuki Wu <wu001474@ahl03.agate.msi.umn.edu>
2026-06-25 17:57:55 -0500

    Add submission script for SLURM

---
+++ submit_jobs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH --time=24:00:00
+#SBATCH --mem=50g
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=wu001474@umn.edu
+#SBATCH -p msismall
+#SBATCH -o /projects/standard/fried255/shared/ForMazuki/logs/slurm-%j.out
+
+module load miniforge
+source activate ../snakemake_env_test
+snakemake --sdm conda \
+--executor slurm \
+--default-resources slurm_account=$1 slurm_partition="msismall" \
+--jobs 4
