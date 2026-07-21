90bee3e5a26b1c47e3757916548dae14f80abd3e
Mazuki <mazuki.wu@gmail.com>
2026-07-21 13:05:37 -0500

    Update slurm submission script

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --time=24:00:00
+#SBATCH --time=96:00:00
 #SBATCH --mem=50g
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=wu001474@umn.edu
@@ -7,9 +7,5 @@
 #SBATCH -o /projects/standard/fried255/shared/ForMazuki/logs/slurm-%j.out
 
 cd /projects/standard/fried255/shared/ForMazuki/CanineContamCheck
-module load miniforge
-source activate ../snakemake_env_test
-snakemake --sdm conda \
---executor slurm \
---default-resources slurm_account=$1 slurm_partition="msismall" \
---jobs 4
+pixi shell
+snakemake --workflow-profile profiles/slurm/profile.yaml
