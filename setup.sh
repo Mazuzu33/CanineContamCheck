341a9000ffd8f1e53dab77a4c3122b35db9c1512
Mazuki <mazuki.wu@gmail.com>
2026-07-27 18:42:26 -0500

    Pull reference genome into GvcfCheck directory

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -l
 wget -P BamCheck/data/reference_genomes/UU_Cfam_GSD_1.0_ROSY https://s3.msi.umn.edu/caninecontamcheck/ref_gen/UU_Cfam_GSD_1.0_ROSY/UU_Cfam_GSD_1.0_ROSY.fa.gz
 wget -P BamCheck/data/reference_genomes/UU_Cfam_GSD_1.0_ROSY https://s3.msi.umn.edu/caninecontamcheck/ref_gen/UU_Cfam_GSD_1.0_ROSY/UU_Cfam_GSD_1.0_ROSY.fa.gz.fai 
+wget -P GvcfCheck/data/reference_genomes/UU_Cfam_GSD_1.0_ROSY https://s3.msi.umn.edu/caninecontamcheck/ref_gen/UU_Cfam_GSD_1.0_ROSY/UU_Cfam_GSD_1.0_ROSY.fa.gz
+wget -P GvcfCheck/data/reference_genomes/UU_Cfam_GSD_1.0_ROSY https://s3.msi.umn.edu/caninecontamcheck/ref_gen/UU_Cfam_GSD_1.0_ROSY/UU_Cfam_GSD_1.0_ROSY.fa.gz.fai 
