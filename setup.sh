1d1e1aebc9d798e63b2faa6416236913f766f9a6
Mazuki <mazuki.wu@gmail.com>
2026-08-08 19:29:02 -0500

    Pull reference panel from MSI

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@ wget -P BamCheck/data/reference_genomes/UU_Cfam_GSD_1.0_ROSY https://s3.msi.umn.
 wget -P BamCheck/data/reference_genomes/UU_Cfam_GSD_1.0_ROSY https://s3.msi.umn.edu/caninecontamcheck/ref_gen/UU_Cfam_GSD_1.0_ROSY/UU_Cfam_GSD_1.0_ROSY.fa.gz.fai 
 wget -P GvcfCheck/data/reference_genomes/UU_Cfam_GSD_1.0_ROSY https://s3.msi.umn.edu/caninecontamcheck/ref_gen/UU_Cfam_GSD_1.0_ROSY/UU_Cfam_GSD_1.0_ROSY.fa.gz
 wget -P GvcfCheck/data/reference_genomes/UU_Cfam_GSD_1.0_ROSY https://s3.msi.umn.edu/caninecontamcheck/ref_gen/UU_Cfam_GSD_1.0_ROSY/UU_Cfam_GSD_1.0_ROSY.fa.gz.fai 
+
+wget -P GvcfCheck/data/reference_panel/UU_Cfam_GSD_1.0_ROSY https://s3.msi.umn.edu/caninecontamcheck/ref_panel/UU_Cfam_GSD_1.0_ROSY/dog_wgs.n3973.UU_Cfam_GSD_1.0_ROSY.20251223.snps.phased.AF.vcf.gz
+wget -P GvcfCheck/data/reference_panel/UU_Cfam_GSD_1.0_ROSY https://s3.msi.umn.edu/caninecontamcheck/ref_panel/UU_Cfam_GSD_1.0_ROSY/dog_wgs.n3973.UU_Cfam_GSD_1.0_ROSY.20251223.snps.phased.AF.vcf.gz.tbi
